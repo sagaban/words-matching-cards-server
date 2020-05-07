04e785b1994430062f49156bfa0e1c93ec1c248a
Santiago Bandiera <sagaban@hotmail.com>
2020-05-07 17:14:07 -0300

    Update client app

---
+++ public/precache-manifest.087458074af74b6034d552ce7d0860c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d718ade338c7a2d481da",
+    "url": "/css/app.ef0c1b6f.css"
+  },
+  {
+    "revision": "88b9118a093ae003ed4f",
+    "url": "/css/chunk-vendors.ece97acf.css"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "d218adb849dc145d1a29c3eed9a36952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f6cd9c4fd550903fb0",
+    "url": "/js/about.83d89c49.js"
+  },
+  {
+    "revision": "d718ade338c7a2d481da",
+    "url": "/js/app.20d9d302.js"
+  },
+  {
+    "revision": "88b9118a093ae003ed4f",
+    "url": "/js/chunk-vendors.d85a3f60.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2bcb2aab6c691f1b8b17fa4b57dcf15.js"
+  "/precache-manifest.087458074af74b6034d552ce7d0860c7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "client"});
